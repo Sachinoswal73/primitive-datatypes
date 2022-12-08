3bc0d46e1692ff500da41f225000002ab18b0131
SachinOswal73 <sachin8973@gmail.com>
2022-12-08 16:00:41 +0530

    taking it to remote server

---
+++ js/app.js
@@ -0,0 +1,78 @@
+
+
+
+    var countryName; // just defined the property, not assigned or initialized
+
+    var stateName = ""; // defined the property & assigned a blank value
+
+    var ct = " (bk)"; // defined & assigned a value
+
+    var cityName = "Pune" + ct; // defined a property & concate another property value to the assigned string
+
+
+    // Datatypes - Primitive - String 
+
+    var fname = "Sachin"; // defined & assigned the value
+
+    console.log(fname);
+
+    var lname = "Oswal";
+
+    var phone = "9028831939";
+
+    var marks = "73.89";
+
+    var address = "B4-201, SPN, Pune.";
+
+
+    // Datatypes - Primitive - Numbers
+
+    var spo = 73; // number - int
+
+    var spo = 73.89; // number - float
+
+    var pmo = 9;
+
+    var spo = spo + 7 + pmo ; // number with some mathematical operation
+
+    var npo = 10;
+
+    var ppo = 12;
+
+    var bpo = 23.95 * 5;
+
+    var sso = 82.68 / 6;
+    
+    var pmo = npo + spo;
+
+    var pmo = npo + ppo;
+
+    total = pmo + npo + ppo + spo;
+
+    // Datatypes - Primitive - Boolean
+
+    var isBlack = true;   
+
+    var isBlack = false;
+
+    var isGoing = false;
+
+    var isGoing = true;
+
+    var fiftyPlus = true; 
+
+    var minus = false;
+
+    var plus = true;
+
+    
+    // Datatypes - Primitive - Undefined
+
+    var ud;
+
+
+    // Datatypes - Primitive - Null
+
+    var udn; // It's just declared but not yet assigned / initialized
+
+    var udn = null; // It's assigned with the null object
